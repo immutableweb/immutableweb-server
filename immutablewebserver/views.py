f956312f96eb341b79dabcb0fa18ce006193cc3d
Robert Kaye <rob@musicbrainz.org>
2018-12-04 22:36:06 +0100

    Implement a super basic get function with no verify.

--- immutablewebserver/views.py
+++ immutablewebserver/views.py
@@ -1,6 +1,46 @@
+import os
+import ujson
 from flask import request, render_template
+from werkzeug.exceptions import NotFound, BadRequest
+
+from immutableweb import stream
 from immutablewebserver import app
+
+
+def validate_stream_id(id):
+    if len(id) != 32:
+        raise BadRequest("Invalid stream id.")
+       
+    try:
+        _ = int(id, 16)
+    except ValueError:
+        raise BadRequest("Invalid stream id.")
+        
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/<id>/<int:block>')
+def uuid_get(id, block):
+
+    validate_stream_id(id)
+    try:
+        send_metadata = int(request.args.get('meta', '1'))
+    except ValueError:
+        raise BadRequest
+
+    try:
+        with stream.Stream(os.path.join(app.config["STREAM_DIR"], id + ".iw")) as s:
+            metadata, block = s.read(block)
+
+            if send_metadata:
+                metadata = bytes(ujson.dumps(metadata), 'utf-8') + b"\n"
+            else:
+                metadata = bytes()
+
+            return metadata + block
+
+    except IOError:
+        raise NotFound("No stream %s available." % id)
--- server.py
+++ server.py
@@ -8,7 +8,9 @@ from immutablewebserver import app
 @click.option('--host', default="0.0.0.0", help='which host IP to bind to.')
 @click.option('--port', default=8080, help='which port to bind to.')
 @click.option('--debug', default=True, help='turn on debug mode')
-def run(host, port, debug):
+@click.argument('streamdir')
+def run(host, port, debug, streamdir):
+    app.config['STREAM_DIR'] = streamdir
     app.run(host=host, port=port, debug=debug) 
 
 
